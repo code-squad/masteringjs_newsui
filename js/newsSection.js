9b81fcaf155ca59bff657a43a40c237d6bbde3f1
jjori.master <jjori.master@gmail.com>
2018-10-12 01:26:56 +0900

    추가 뉴스 섹션 초기화 함수에 언론사 목록과, 뉴스목록 첫번째를 보여주도록 셋팅

--- js/newsSection.js
+++ js/newsSection.js
@@ -1,8 +1,12 @@
+import {newslist as newsList} from '../data/newslist.js';
+
 export default class newsSection {
     constructor() {
     }
 
     init(fnNewsListTemplate, fnNewsCompanyList) {
+        document.querySelector('.content').innerHTML = fnNewsListTemplate(newsList[0]);
+        document.querySelector('.newsNavigation').innerHTML = fnNewsCompanyList(newsList).join('\n');
     }
 
 }
